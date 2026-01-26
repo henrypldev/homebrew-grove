ada4ae29ae08cc14785795059a20e7a17817c926
Henry Paulino <me@henrypl.com>
2026-01-26 14:11:29 +0000

    grove 0.1.0

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -1,13 +1,13 @@
 class Grove < Formula
   desc "Mobile terminal server for Claude Code - manage sessions from your phone"
   homepage "https://github.com/henrypl/grove"
-  version "1.0.0"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/henrypl/grove/releases/download/v#{version}/grove-#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "9842dfe7e65dbc43bbe344d56923b62bbf88c35165979684b75854a2371760b8"
 
       def install
         bin.install "grove-#{version}-darwin-arm64" => "grove"
@@ -16,7 +16,7 @@ class Grove < Formula
 
     on_intel do
       url "https://github.com/henrypl/grove/releases/download/v#{version}/grove-#{version}-darwin-x64.tar.gz"
-      sha256 "PLACEHOLDER_X64_SHA256"
+      sha256 "24b8dd49aa6bd454ebfa43ff15471fc8fdb647a9f79bf29a762b94bc6e2590a3"
 
       def install
         bin.install "grove-#{version}-darwin-x64" => "grove"
